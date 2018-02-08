7426a765edb8ec88d07b47490cae4e12c6a3449b
haotian <wanghaotian@apass.cn>
2018-02-08 17:53:08 +0800

    pingpai

--- zufang-ams/src/main/java/com/apass/zufang/web/apartment/ApartmentController.java
+++ zufang-ams/src/main/java/com/apass/zufang/web/apartment/ApartmentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.apass.gfb.framework.security.toolkit.SpringSecurityUtils;
 import com.apass.zufang.domain.Response;
 import com.apass.zufang.domain.entity.Apartment;
-import com.apass.zufang.service.ApartmentService;
+import com.apass.zufang.service.apartment.ApartmentService;
 import com.apass.zufang.utils.ResponsePageBody;
 /**
  * 公寓信息管理
@@ -39,15 +39,16 @@ public class ApartmentController {
     public ResponsePageBody<Apartment> getApartmentList(Apartment entity) {
         ResponsePageBody<Apartment> respBody = new ResponsePageBody<Apartment>();
         try {
+        	entity.setIsDelete("00");
         	respBody = apartmentService.getApartmentList(entity);
         } catch (Exception e) {
-            LOGGER.error("公寓信息列表查询失败", e);
+            LOGGER.error("getApartmentList EXCEPTION --- --->{}", e);
             respBody.setMsg("公寓信息列表查询失败");
         }
         return respBody;
     }
     /**
-     * 保存公寓
+     * 公寓信息新增
      * @param entity
      * @return
      */
@@ -58,7 +59,24 @@ public class ApartmentController {
 			String username = SpringSecurityUtils.getCurrentUser();
 			return apartmentService.addApartment(entity,username);
 		}catch(Exception e){
-			return null;
+			LOGGER.error("addApartment EXCEPTION --- --->{}", e);
+			return Response.fail("公寓信息新增失败！");
+		}
+	}
+    /**
+     * 公寓信息修改
+     * @param entity
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/editApartment")
+	public Response editApartment(@RequestBody Apartment entity){
+		try{
+			String username = SpringSecurityUtils.getCurrentUser();
+			return apartmentService.editApartment(entity,username);
+		}catch(Exception e){
+			LOGGER.error("editApartment EXCEPTION --- --->{}", e);
+			return Response.fail("公寓信息修改失败！");
 		}
 	}
 }
