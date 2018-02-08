4e5bd2514d24b9257aeb0b93c370ce70ff35fb40
xujie <xujie@apass.cn>
2018-02-08 10:41:11 +0800

    1

--- zufang-ams/src/main/java/com/apass/zufang/Application.java
+++ zufang-ams/src/main/java/com/apass/zufang/Application.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import com.apass.gfb.framework.BootApplication;
 
 @Configuration
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
