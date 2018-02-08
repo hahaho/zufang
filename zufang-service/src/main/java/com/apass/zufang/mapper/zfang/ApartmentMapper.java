03a0fa7f03dfbac962a68a56527e3e4811dd31f7
haotian <wanghaotian@apass.cn>
2018-02-08 16:30:57 +0800

    gongyu

--- zufang-service/src/main/java/com/apass/zufang/mapper/zfang/ApartmentMapper.java
+++ zufang-service/src/main/java/com/apass/zufang/mapper/zfang/ApartmentMapper.java
@@ -1,10 +1,18 @@
 package com.apass.zufang.mapper.zfang;
-
+import java.util.List;
 import com.apass.gfb.framework.mybatis.GenericMapper;
 import com.apass.zufang.domain.entity.Apartment;
-
-/**
- * Created by DELL on 2018/2/7.
- */
 public interface ApartmentMapper extends GenericMapper<Apartment,Long> {
+	/**
+	 * getApartmentList
+	 * @param entity
+	 * @return
+	 */
+	public List<Apartment> getApartmentList(Apartment entity);
+	/**
+	 * getApartmentListCount
+	 * @param entity
+	 * @return
+	 */
+	public Integer getApartmentListCount(Apartment entity);
 }
--- zufang-service/src/main/java/com/apass/zufang/service/ApartmentService.java
+++ zufang-service/src/main/java/com/apass/zufang/service/ApartmentService.java
@@ -1,6 +1,10 @@
 package com.apass.zufang.service;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.apass.gfb.framework.utils.BaseConstants;
 import com.apass.zufang.domain.Response;
 import com.apass.zufang.domain.entity.Apartment;
 import com.apass.zufang.mapper.zfang.ApartmentMapper;
@@ -14,6 +18,7 @@ import com.apass.zufang.utils.ResponsePageBody;
 public class ApartmentService {
 	@Autowired
 	private ApartmentMapper apartmentMapper;
+	@Transactional(rollbackFor = { Exception.class})
 	public Integer createEntity(Apartment entity){
 		return apartmentMapper.insertSelective(entity);
 	}
@@ -23,18 +28,29 @@ public class ApartmentService {
 	public Apartment readEntity(Long id){
 		return apartmentMapper.selectByPrimaryKey(id);
 	}
+	@Transactional(rollbackFor = { Exception.class})
 	public Integer updateEntity(Apartment entity){
 		return apartmentMapper.updateByPrimaryKeySelective(entity);
 	}
+	@Transactional(rollbackFor = { Exception.class})
 	public Integer deleteEntity(Apartment entity){
 		return apartmentMapper.deleteByPrimaryKey(entity.getId());
 	}
+	@Transactional(rollbackFor = { Exception.class})
 	public Integer deleteEntity(Long id){
 		return apartmentMapper.deleteByPrimaryKey(id);
 	}
 	public ResponsePageBody<Apartment> getApartmentList(Apartment entity) {
-		return null;
+		entity.setIsDelete("00");
+		ResponsePageBody<Apartment> pageBody = new ResponsePageBody<Apartment>();
+        Integer count = apartmentMapper.getApartmentListCount(entity);
+        List<Apartment> response = apartmentMapper.getApartmentList(entity);
+        pageBody.setTotal(count);
+        pageBody.setRows(response);
+        pageBody.setStatus(BaseConstants.CommonCode.SUCCESS_CODE);
+        return pageBody;
 	}
+	@Transactional(rollbackFor = { Exception.class})
 	public Response addApartment(Apartment entity,String username) {
 		return Response.success("公寓信息新增成功！");
 	}
