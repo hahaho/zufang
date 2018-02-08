56d56c5d70fe8494c17276dbb906d782a38ff13f
liaoshun <liaoshun@apass.cn>
2018-02-08 16:26:38 +0800

    zufanglogin

---
+++ zufang-search/src/main/java/com/apass/zufang/search/dao/ZuFangLoginDao.java
@@ -0,0 +1,19 @@
+package com.apass.zufang.search.dao;
+
+import com.apass.zufang.domain.ajp.entity.ZuFangLogin;
+
+public class ZuFangLoginDao {
+
+	public Integer zufangsetpassword(ZuFangLogin zuFangLogin) {
+		return null;
+		
+	}
+
+	public Integer zufangpasswordlogin(ZuFangLogin zuFangLogin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
