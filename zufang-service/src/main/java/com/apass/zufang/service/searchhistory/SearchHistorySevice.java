fb4819810f86c8f49e630397aa9c1c099324d208
liaoshun <liaoshun@apass.cn>
2018-02-09 17:18:08 +0800

    ““搜索历史”

---
+++ zufang-service/src/main/java/com/apass/zufang/service/searchhistory/SearchHistorySevice.java
@@ -0,0 +1,42 @@
+package com.apass.zufang.service.searchhistory;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.apass.zufang.mapper.ajp.searchhistory.SearchHistoryDao;
+
+/**
+ * 搜索历史
+ * 
+ * @author ls
+ * @update 2018-02-09 11:02
+ *
+ */
+@Component
+public class SearchHistorySevice<T> {
+	
+	@Autowired
+	private SearchHistoryDao searchHistoryDao;
+	
+	/**
+	 * 设备ID查询
+	 * @param deviceId
+	 * @return
+	 */
+	public List<T> queryDeviceIdHistory(String deviceId) {
+		return  searchHistoryDao.queryDeviceIdHistory(deviceId);
+	}
+	/**
+	 * 用户ID查询
+	 * @param customerId
+	 * @return
+	 */ 
+	public List<T> queryCustomerIdHistory(String customerId) {
+		return searchHistoryDao.queryCustomerIdHistory(customerId);
+		
+	}
+
+	
+}
