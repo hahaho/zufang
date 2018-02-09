5965079501f6dd851723dd10c908133eea55d111
sunchaohai <sunchaohai@apass.cn>
2018-02-09 16:08:07 +0800

    启动报错....

--- zufang-search/src/main/java/com/apass/zufang/search/dao/ZuFangLoginDao.java
+++ zufang-search/src/main/java/com/apass/zufang/search/dao/ZuFangLoginDao.java
@@ -1,7 +1,10 @@
 package com.apass.zufang.search.dao;
 
+import com.apass.gfb.framework.annotation.MyBatisEntity;
+import com.apass.gfb.framework.annotation.MyBatisRepository;
 import com.apass.zufang.domain.ajp.entity.ZuFangLogin;
 
+@MyBatisRepository
 public class ZuFangLoginDao {
 
 	public Integer zufangsetpassword(ZuFangLogin zuFangLogin) {
