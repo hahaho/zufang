3ae22d7b1d76e65ef6215608080f7068a1458b2d
jiangxiangwei <jiangxiangwei@jiangxiangwei.vcredit.cn>
2018-02-09 16:22:08 +0800

    附近房源需求

--- zufang-service/src/main/java/com/apass/zufang/service/commons/CommonService.java
+++ zufang-service/src/main/java/com/apass/zufang/service/commons/CommonService.java
@@ -1,6 +1,7 @@
 package com.apass.zufang.service.commons;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CommonService {
@@ -8,7 +9,7 @@ public class CommonService {
 	/** 
      * 默认地球半径 
      */  
-    private static double EARTH_RADIUS = 6378.140;  
+    private static double EARTH_RADIUS = 6367000.0;  //  单位：m
   
     
     /** 
@@ -43,6 +44,35 @@ public class CommonService {
         squareMap.put("rightBottomPoint", rightBottomPoint);  
         return squareMap;  
     } 
-	
-	
+    
+    /**
+     * 获取两个经纬度点的距离
+     * 
+     * @param goalLat 目的地纬度
+     * @param goalLng 目的地经度
+     * @param lat  附近房源纬度
+     * @param lng  附近房源经度
+     * @return
+     */
+    public double distanceSimplify(double goalLat, double goalLng, double lat, double lng) {
+        double dx = goalLng - lng; // 经度差值
+        double dy = goalLat - lat; // 纬度差值
+        double b = (goalLat + lat) / 2.0; // 平均纬度
+        double Lx = Math.toRadians(dx) * EARTH_RADIUS* Math.cos(Math.toRadians(b)); // 东西距离
+        double Ly = EARTH_RADIUS * Math.toRadians(dy); // 南北距离
+        return Math.sqrt(Lx * Lx + Ly * Ly);  // 用平面的矩形对角距离公式计算总距离
+       }
+	/**
+	 * 获取附近房源
+	 * 
+	 * @return
+	 */
+    public List<String> getNearbyListings(){
+    	
+    	
+    	
+    	
+		return null;
+    }
+    
 }
