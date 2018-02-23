c0dc8d69c23120cea4f953c67b5a8fca1ee5e109
haotian <wanghaotian@apass.cn>
2018-02-23 16:37:09 +0800

    房源图片表新增图片类型字段，默认0标识房源普通图片，1标识房源热门专属图片

--- zufang-service/src/main/java/com/apass/zufang/mapper/zfang/HouseImgMapper.java
+++ zufang-service/src/main/java/com/apass/zufang/mapper/zfang/HouseImgMapper.java
@@ -1,14 +1,15 @@
 package com.apass.zufang.mapper.zfang;
-
 import java.util.List;
-
 import com.apass.gfb.framework.mybatis.GenericMapper;
 import com.apass.zufang.domain.entity.HouseImg;
-
 /**
  * Created by DELL on 2018/2/7.
  */
 public interface HouseImgMapper extends GenericMapper<HouseImg,Long>{
-	
-	List<HouseImg> getImgByHouseId(Long houseId);
+	/**
+	 * getImgByHouseId
+	 * @param houseImg
+	 * @return
+	 */
+	public List<HouseImg> getImgByHouseId(HouseImg houseImg);
 }
