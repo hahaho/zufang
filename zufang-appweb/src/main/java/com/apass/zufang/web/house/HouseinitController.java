b6ccc6f3b6b72c4feb98e9f6cc63130c87cc3f05
tanshilin <tanshilin@apass.cn>
2018-02-24 15:08:18 +0800

    初始首页

--- zufang-appweb/src/main/java/com/apass/zufang/web/house/HouseinitController.java
+++ zufang-appweb/src/main/java/com/apass/zufang/web/house/HouseinitController.java
@@ -1,5 +1,6 @@
 package com.apass.zufang.web.house;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -12,14 +13,17 @@ import javax.ws.rs.core.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.apass.gfb.framework.logstash.LOG;
+import com.apass.gfb.framework.utils.BaseConstants;
 import com.apass.gfb.framework.utils.CommonUtils;
 import com.apass.gfb.framework.utils.GsonUtils;
 import com.apass.zufang.domain.Response;
 import com.apass.zufang.domain.entity.Apartment;
-import com.apass.zufang.domain.entity.HouseInfoRela;
+import com.apass.zufang.domain.entity.HouseLocation;
+import com.apass.zufang.domain.vo.HouseVo;
+import com.apass.zufang.service.commons.CommonService;
 import com.apass.zufang.service.house.HouseinitService;
-
-import cn.jpush.api.utils.StringUtils;
+import com.apass.zufang.utils.ResponsePageBody;
+import com.apass.zufang.utils.ValidateUtils;
 
 
 @Path("/home")
@@ -31,46 +35,103 @@ public class HouseinitController {
     private HouseinitService houseinitService;
 	
     /**
-     * 初始app地址
+     * init城市
      * @param paramMap
      * @return
      */
 	@POST
-	@Path("/init")
-	public Response handleAdvanceStatus(Map<String, Object> paramMap) {
-//		String city = CommonUtils.getValue(paramMap, "city");// 城市
+	@Path("/initCity")
+	public Response handleAdvanceStatus() {
 		try {
-			return Response.success("success");
+			// 获取市区
+			ArrayList<String> arrayList = new ArrayList<>();
+//			houseinitService.initCity();
+			return Response.success("初始城市地址成功!", GsonUtils.toJson(arrayList));
 		} catch (Exception e) {
-			LOG.error("初始app地址失败", e);
-			return Response.fail("初始app地址失败");
+			LOG.error("初始城市地址失败", e);
+			return Response.fail("初始城市地址失败");
 		}
 	}
 
 	/**
-	 * 搜索
+	 * init热门房源
 	 * @param paramMap
 	 * @return
 	 */
 	@POST
-	@Path("/search")
-	public Response getSerach(Map<String, Object> paramMap) {
+	@Path("/initHotHouseList")
+	public Response initHotHouse(Map<String, Object> paramMap) {
 		
+		ResponsePageBody<HouseVo> pageBody = new ResponsePageBody<>();
 		try {
 			
-			String city = CommonUtils.getValue(paramMap, "city");// 区域
-			Long houseId = CommonUtils.getLong(paramMap, "houseId");// 房源名
-			String district = CommonUtils.getValue(paramMap, "district");// 小区
+			String city = CommonUtils.getValue(paramMap, "city");// 城市
+			String province = CommonUtils.getValue(paramMap, "province");// 省份
+			String district = CommonUtils.getValue(paramMap, "district");// 区域
+			ValidateUtils.isNotBlank("请求参数丢失数据", city, province, district);
 			
-			HouseInfoRela paramHouse = new HouseInfoRela();
-			paramHouse.setDistrict(district);
-			paramHouse.setCity(city);
-			paramHouse.setHouseId(houseId);
-//			List<Apartment> results = houseinitService.getHourseInfos(paramHouse);
-			return Response.success("success");
+			HouseLocation houseLocation = new HouseLocation();
+			houseLocation.setCity("%"+city+"%");
+			houseLocation.setProvince("%"+province+"%");
+			houseLocation.setDistrict("%"+district+"%");
+			
+			// 从后台配置读取热门房源
+			List<HouseVo> searchPeizhi = houseinitService.initPeizhiLocation(houseLocation);
+			if(searchPeizhi!=null && !searchPeizhi.isEmpty()){
+				//不为空的情况
+				if (searchPeizhi.size() == 6) {
+					pageBody.setRows(searchPeizhi);
+					pageBody.setTotal(searchPeizhi.size());
+					pageBody.setStatus(BaseConstants.CommonCode.SUCCESS_CODE);
+					pageBody.setMsg("获取配置房源");
+				}else {
+					List<HouseVo> hotHouseList = houseinitService.initHotLocation(houseLocation);
+					for (int i=0; i<6-searchPeizhi.size(); i++) {
+						searchPeizhi.add(hotHouseList.get(i));
+					}
+				}
+				}else{
+				//为空的情况 按照按浏览量（7天）降序排列房源
+				List<HouseVo> hotHouseList = houseinitService.initHotLocation(houseLocation);
+				pageBody.setRows(hotHouseList);
+				pageBody.setTotal(hotHouseList.size());
+				pageBody.setStatus(BaseConstants.CommonCode.SUCCESS_CODE);
+				pageBody.setMsg("获取热门房源");
+				}
+			
+			return Response.success("初始热门房源成功！", GsonUtils.toJson(pageBody));
 		} catch (Exception e) {
-			LOG.error("搜索房源失败", e);
-			return Response.fail("搜索房源失败");
+			LOG.error("初始热门房源失败！", e);
+			return Response.fail("初始热门房源失败！");
+		}
+	}
+	/**
+	 * init附近房源
+	 * @param paramMap
+	 * @return
+	 */
+	@POST
+	@Path("/initNearHouseList")
+	public Response initNearHouse(Map<String, Double> paramMap) {
+		
+		ResponsePageBody<HouseVo> pageBody = new ResponsePageBody<HouseVo>();
+		try {
+			
+			// 获取用户当前经纬度追加2公里经纬度,
+			Double longitude = paramMap.get("longitude");// 经度
+			Double latitude = paramMap.get("latitude");// 维度
+			Map<String, Double> returnLLSquarePoint = CommonService.renturnLngLat(longitude, latitude, new Double(2000));
+			
+			List<HouseVo> initNearHouse = houseinitService.initNearLocation(returnLLSquarePoint);
+			pageBody.setRows(initNearHouse);
+			pageBody.setStatus(BaseConstants.CommonCode.SUCCESS_CODE);
+			pageBody.setTotal(initNearHouse.size());
+			pageBody.setMsg("初始附近房源");
+			
+			return Response.success("初始附近房源成功！", GsonUtils.toJson(initNearHouse));
+		} catch (Exception e) {
+			LOG.error("初始附近房源失败！", e);
+			return Response.fail("初始附近房源失败！");
 		}
 	}
 	
@@ -82,16 +143,22 @@ public class HouseinitController {
 	@Path("/getApartGongyu")
 	public Response getApartGongyu(Map<String, Object> paramMap) {
 		
+		ResponsePageBody<Apartment> pageBody = new ResponsePageBody<>();
 		try {
+			
 			String communityName = CommonUtils.getValue(paramMap, "communityName");// 区域
-			LOG.info(GsonUtils.toJson(paramMap));
-			if (StringUtils.isEmpty(communityName)) {
-				return Response.fail("品牌名称无数据");
-			}
+			ValidateUtils.isNotBlank(communityName, "品牌名称无数据");
+			
 			Apartment apartment = new Apartment();
 			apartment.setCompanyName(communityName);
+			
 			List<Apartment> resultApartment = houseinitService.getApartGongyu(apartment);
-			return Response.success("success", resultApartment);
+			pageBody.setRows(resultApartment);
+			pageBody.setStatus(BaseConstants.CommonCode.SUCCESS_CODE);
+			pageBody.setTotal(resultApartment.size());
+			pageBody.setMsg("获取品牌公寓");
+			
+			return Response.success("success", pageBody);
 		} catch (Exception e) {
 			LOG.error("查询品牌公寓失败", e);
 			return Response.fail("查询品牌公寓失败");
--- zufang-service/src/main/java/com/apass/zufang/service/house/HouseinitService.java
+++ zufang-service/src/main/java/com/apass/zufang/service/house/HouseinitService.java
@@ -1,18 +1,25 @@
 package com.apass.zufang.service.house;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.apass.zufang.domain.entity.Apartment;
+import com.apass.zufang.domain.entity.HouseLocation;
+import com.apass.zufang.domain.vo.HouseVo;
 import com.apass.zufang.mapper.zfang.ApartmentMapper;
+import com.apass.zufang.mapper.zfang.HouseLocationMapper;
+import com.apass.zufang.mapper.zfang.HouseMapper;
 
 @Component
 public class HouseinitService {
 	
+	@Autowired
+	private ApartmentMapper apartmentMapper;
     @Autowired
-    private ApartmentMapper apartmentMapper;
+    private HouseLocationMapper HouseLocationMapper;
 
 	/**
 	 * 品牌公寓接口:默认按创建降序排列20个
@@ -20,6 +27,37 @@ public class HouseinitService {
 	 */
 	public List<Apartment> getApartGongyu(Apartment apartment) {
 		return apartmentMapper.getApartGongyu(apartment);
+	}
+
+	/**
+	 * init配置热门房源
+	 * @param houseLocation 
+	 * @return
+	 */
+	public List<HouseVo> initPeizhiLocation(HouseLocation houseLocation) {
+		
+		List<HouseVo> initPei = HouseLocationMapper.initPeizhiLocation(houseLocation);
+		return initPei;
+	}
+
+	/**
+	 * init热门房源
+	 * @param houseLocation 
+	 * @return
+	 */
+	public List<HouseVo> initHotLocation(HouseLocation houseLocation) {
+		List<HouseVo> inithot = HouseLocationMapper.initHotLocation(houseLocation);
+		return inithot;
+	}
+
+	/**
+	 * init附近房源
+	 * @param returnLLSquarePoint
+	 * @return
+	 */
+	public List<HouseVo> initNearLocation(Map<String, Double> returnLLSquarePoint) {
+		List<HouseVo> initNearHouse = HouseLocationMapper.initNearLocation(returnLLSquarePoint);
+		return initNearHouse;
 	}
 
 }
