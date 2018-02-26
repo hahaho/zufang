e73b6d737456a074d68e18529918e42026ed7ebb
xujie <xujie@apass.cn>
2018-02-26 15:30:23 +0800

    sql

---
+++ zufang-service/src/main/java/com/apass/zufang/mapper/zfang/ReserveHouseMapper.java
@@ -0,0 +1,10 @@
+package com.apass.zufang.mapper.zfang;
+
+import com.apass.gfb.framework.mybatis.GenericMapper;
+import com.apass.zufang.domain.entity.ReserveHouse;
+
+/**
+ * Created by DELL on 2018/2/26.
+ */
+public interface ReserveHouseMapper extends GenericMapper<ReserveHouse,Long> {
+}
