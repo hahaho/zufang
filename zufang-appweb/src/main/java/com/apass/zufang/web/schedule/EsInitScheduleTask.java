612627be558f2b095421ccca4b9ac4781dc6a3ab
sunchaohai <sunchaohai@apass.cn>
2018-02-27 17:27:21 +0800

    加@Reposity

--- zufang-appweb/src/main/java/com/apass/zufang/web/schedule/EsInitScheduleTask.java
+++ zufang-appweb/src/main/java/com/apass/zufang/web/schedule/EsInitScheduleTask.java
@@ -9,11 +9,6 @@ import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.Profile;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
