2e64bae52e5b32e891100138231a952e49b4a48d
sunchaohai <sunchaohai@apass.cn>
2018-02-28 18:05:36 +0800

    注释掉加密拦截器....

--- zufang-ams/src/main/java/com/apass/zufang/web/rbac/RolesController.java
+++ zufang-ams/src/main/java/com/apass/zufang/web/rbac/RolesController.java
@@ -16,9 +16,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
