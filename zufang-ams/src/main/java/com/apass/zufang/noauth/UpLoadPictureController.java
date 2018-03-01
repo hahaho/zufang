577c865f7a1fd81a53f3360ea3298f9dd8062855
haotian <wanghaotian@apass.cn>
2018-03-01 18:39:57 +0800

    1111

--- zufang-ams/src/main/java/com/apass/zufang/noauth/UpLoadPictureController.java
+++ zufang-ams/src/main/java/com/apass/zufang/noauth/UpLoadPictureController.java
@@ -1,14 +1,15 @@
 package com.apass.zufang.noauth;
-
+import java.io.IOException;
+import java.io.InputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.apass.gfb.framework.exception.BusinessException;
 import com.apass.zufang.domain.Response;
 import com.apass.zufang.utils.FileUtilsCommons;
@@ -16,17 +17,13 @@ import com.apass.zufang.utils.ImageTools;
 @Controller
 @RequestMapping("/application/")
 public class UpLoadPictureController {
-
 	private static final Logger logger = LoggerFactory.getLogger(UpLoadPictureController.class);
-	
 	/** * 图片服务器地址*/
     @Value("${nfs.rootPath}")
     private String rootPath;
-    
     /*** 房屋图片存放地址*/
     @Value("${nfs.house}")
     private String nfsHouse;
-	
 	@ResponseBody
     @RequestMapping(value = "/uppicture", method = RequestMethod.POST)
 	public Response uploadPicture(MultipartFile file){
@@ -56,6 +53,48 @@ public class UpLoadPictureController {
 		}catch (Exception e) {
 			logger.error("上传house logo失败!", e);
             return Response.fail("上传商品大图失败!");
+        }
+    }
+	/**
+     * 编辑精选商品的排序和图片
+     * @param siftGoodFileModel
+     * @return
+	 * @throws BusinessException 
+     */
+    @ResponseBody
+    @RequestMapping(value = "/upLoadCompanyLogo", method = RequestMethod.POST)
+    public Response upLoadCompanyLogo(@ModelAttribute("file") MultipartFile file) {
+        InputStream is = null;
+        try {
+        	if(file == null){
+        		throw new BusinessException("上传图片为空!");
+        	}
+            is = file.getInputStream();
+            int size = is.available();// 大小
+            String imgType = ImageTools.getImgType(file);
+            String fileName = "companyLogo_" + System.currentTimeMillis()+ "." + imgType;
+            String url = nfsHouse + fileName;
+            //图片校验
+            boolean checkSiftGoodsImgSize = ImageTools.checkSiftGoodsImgSize(file);// 尺寸
+            boolean checkImgType = ImageTools.checkImgType(file);// 类型
+            if (!(checkSiftGoodsImgSize && checkImgType)) {
+                return Response.fail("图片尺寸格式不符,尺寸要求:120*120,格式要求：.jpg,.png");
+            } else if (size > 1024 * 500) {
+                return Response.fail("图片不能大于501kb!");
+            }
+            FileUtilsCommons.uploadFilesUtil(rootPath, url, file);
+            return Response.success("上传公司LOGO成功！",url);
+        } catch (Exception e) {
+        	logger.error("upLoadCompanyLogo Exception---->{}",e);
+            return Response.fail("上传公司LOGO失败!");
+        }finally{
+            try {
+                if(is!=null){
+                    is.close();
+                }
+            } catch (IOException e) {
+                logger.error("IOException businessException---->{}",e);
+            }
         }
     }
 }
