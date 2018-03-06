baa47f42c0f727279fc68e717bd193c1983683fc
zhanwendong <zhanwendong@apass>
2018-03-06 16:45:15 +0800

    0000

--- zufang-appweb/src/main/java/com/apass/zufang/web/house/SearchTermsController.java
+++ zufang-appweb/src/main/java/com/apass/zufang/web/house/SearchTermsController.java
@@ -16,6 +16,7 @@ import com.apass.gfb.framework.exception.BusinessException;
 import com.apass.gfb.framework.logstash.LOG;
 import com.apass.gfb.framework.utils.CommonUtils;
 import com.apass.zufang.domain.Response;
+import com.apass.zufang.domain.common.KeyValue;
 import com.apass.zufang.domain.common.WorkCityJd;
 import com.apass.zufang.domain.entity.Apartment;
 import com.apass.zufang.domain.entity.WorkSubway;
@@ -27,6 +28,8 @@ import com.apass.zufang.service.house.ApartHouseService;
 import com.apass.zufang.service.searchhistory.WorkSubwaySevice;
 import com.apass.zufang.utils.ValidateUtils;
 import com.google.common.collect.Maps;
+
+import net.sf.json.JSONArray;
 
 /**
  * 搜索项接口
@@ -45,7 +48,6 @@ public class SearchTermsController {
 	@Autowired
 	private WorkSubwaySevice workSubwaySevice;
 
-
 	/**
 	 * 品牌公寓
 	 * 
@@ -60,7 +62,7 @@ public class SearchTermsController {
 
 			Apartment apartment = new Apartment();
 			apartment.setCity(city);
-			
+
 			List<Apartment> resultApartment = apartHouseService.getApartmentBylistCity(apartment);
 
 			return Response.success("success", resultApartment);
@@ -82,21 +84,20 @@ public class SearchTermsController {
 	@Path("/getPrice")
 	public Response getPrice(Map<String, Object> paramMap) {
 		try {
-			
-			Map<Integer,Object> resultMap=Maps.newHashMap();
-			
+
+			Map<Integer, Object> resultMap = Maps.newHashMap();
+
 			PriceRangeEnum[] result = PriceRangeEnum.values();
 			for (int i = 0; i < result.length; i++) {
 				resultMap.put(result[i].getVal(), result[i].getDesc());
 			}
-			
+
 			return Response.success("success", resultMap);
 		} catch (Exception e) {
 			LOG.error("查询价格失败！", e);
 			return Response.fail("查询价格失败！");
 		}
 	}
-
 
 	/**
 	 * 查询位置区域及地铁线路
@@ -108,31 +109,32 @@ public class SearchTermsController {
 	public Response getSubwayLineAndSiteOrAre(Map<String, Object> paramMap) {
 		try {
 			String code = CommonUtils.getValue(paramMap, "code");// 当前位置的编码
-			
+
 			ValidateUtils.isNotBlank(code, "编码无数据");
 
-			WorkSubway domin=new WorkSubway();
-			domin.setParentCode(Long.valueOf(code));;
+			WorkSubway domin = new WorkSubway();
+			domin.setParentCode(Long.valueOf(code));
+			;
 			/**
 			 * 获取地铁及其站点
 			 */
-			List<WorkSubway> result=workSubwaySevice.getSubwayLineAndSiteOrAre(domin);
+			List<WorkSubway> result = workSubwaySevice.getSubwayLineAndSiteOrAre(domin);
 			/**
 			 * 获取位置区域
 			 */
 			List<WorkCityJd> resultJd = workSubwaySevice.queryCityJdParentCodeList(code);
-			
-			List<WorkSubway> resultJdJson=new ArrayList<WorkSubway>();
-			
+
+			List<WorkSubway> resultJdJson = new ArrayList<WorkSubway>();
+
 			for (WorkCityJd workCityJd : resultJd) {
-				List<WorkSubway> resultJdJsonTemp=new ArrayList<WorkSubway>();
-				WorkSubway temp=new WorkSubway(); 
+				List<WorkSubway> resultJdJsonTemp = new ArrayList<WorkSubway>();
+				WorkSubway temp = new WorkSubway();
 				temp.setId(workCityJd.getId());
 				temp.setCode(Long.valueOf(workCityJd.getCode()));
 				temp.setLineName(workCityJd.getCity());
-				if(!workCityJd.getResultList().isEmpty()){
+				if (!workCityJd.getResultList().isEmpty()) {
 					for (WorkCityJd workSubwaysss : workCityJd.getResultList()) {
-						WorkSubway tempsss=new WorkSubway();
+						WorkSubway tempsss = new WorkSubway();
 						temp.setId(workSubwaysss.getId());
 						tempsss.setCode(Long.valueOf(workSubwaysss.getCode()));
 						tempsss.setSiteName(workSubwaysss.getDistrict());
@@ -142,8 +144,8 @@ public class SearchTermsController {
 				}
 				resultJdJson.add(temp);
 			}
-			
-			Map<String,Object> resultMap=Maps.newHashMap();
+
+			Map<String, Object> resultMap = Maps.newHashMap();
 			resultMap.put("workSubway", result);
 			resultMap.put("workCityJd", resultJdJson);
 
@@ -156,7 +158,7 @@ public class SearchTermsController {
 			return Response.fail("查询位置区域及地铁线路失败！");
 		}
 	}
-	
+
 	/**
 	 * 筛选
 	 * 
@@ -166,34 +168,50 @@ public class SearchTermsController {
 	@Path("/getSelectFilter")
 	public Response getSelectFilter(Map<String, Object> paramMap) {
 		try {
-			
-			Map<String,Object> resultMap=Maps.newHashMap();
-			//类型 合租 整租
-			Map<Integer,Object> sharedType=Maps.newHashMap();
-			sharedType.put(BusinessHouseTypeEnums.HZ_1.getCode(), BusinessHouseTypeEnums.HZ_1.getMessage());
-			sharedType.put(BusinessHouseTypeEnums.HZ_2.getCode(), BusinessHouseTypeEnums.HZ_2.getMessage());
-			//户型
-			Map<Integer,Object> huxingType=Maps.newHashMap();
+
+			Map<String, Object> resultMap = Maps.newHashMap();
+			// 类型 合租 整租
+			List<KeyValue> sharedType = new ArrayList<KeyValue>();
+			KeyValue HZ_1 = new KeyValue();
+			HZ_1.setKey(BusinessHouseTypeEnums.HZ_1.getCode());
+			HZ_1.setValue(BusinessHouseTypeEnums.HZ_1.getMessage());
+			sharedType.add(HZ_1);
+			KeyValue HZ_2 = new KeyValue();
+			HZ_2.setKey(BusinessHouseTypeEnums.HZ_2.getCode());
+			HZ_2.setValue(BusinessHouseTypeEnums.HZ_2.getMessage());
+			sharedType.add(HZ_2);
+			JSONArray sharedTypeJsonArray = JSONArray.fromObject(sharedType);
+
+			// 户型
+			List<KeyValue> huxingType = new ArrayList<KeyValue>();
 			HuxingEnums[] resultHuxingEnums = HuxingEnums.values();
 			for (int i = 0; i < resultHuxingEnums.length; i++) {
-				huxingType.put(resultHuxingEnums[i].getCode(), resultHuxingEnums[i].getMessage());
+				KeyValue keyValue = new KeyValue();
+				keyValue.setKey(resultHuxingEnums[i].getCode());
+				keyValue.setValue(resultHuxingEnums[i].getMessage());
+				huxingType.add(keyValue);
 			}
-			//特色配置
-			Map<Integer,Object> FeaturesConfigurationType=Maps.newHashMap();
+			JSONArray huxingTypeJsonArray = JSONArray.fromObject(huxingType);
+			// 特色配置
+			List<KeyValue> features = new ArrayList<KeyValue>();
 			FeaturesConfigurationEnums[] featuresEnums = FeaturesConfigurationEnums.values();
 			for (int i = 0; i < featuresEnums.length; i++) {
-				FeaturesConfigurationType.put(featuresEnums[i].getCode(), featuresEnums[i].getMessage());
+				KeyValue keyValue = new KeyValue();
+				keyValue.setKey(featuresEnums[i].getCode());
+				keyValue.setValue(featuresEnums[i].getMessage());
+				features.add(keyValue);
 			}
-			
-			resultMap.put("sharedType", sharedType);
-			resultMap.put("roomType", huxingType);
-			resultMap.put("FeaturesConfiguration", FeaturesConfigurationType);
-			
+			JSONArray featuresJsonArray = JSONArray.fromObject(features);
+
+			resultMap.put("sharedType", sharedTypeJsonArray);
+			resultMap.put("roomType", huxingTypeJsonArray);
+			resultMap.put("FeaturesConfiguration", featuresJsonArray);
+
 			return Response.success("success", resultMap);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			LOG.error("查询位置区域及地铁线路失败！", e);
 			return Response.fail("查询位置区域及地铁线路失败！");
 		}
 	}
-	
+
 }
