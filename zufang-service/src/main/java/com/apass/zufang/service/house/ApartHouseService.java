967dbff5986c96a9d6b37fe220f4f79e2014520f
tanshilin <tanshilin@apass.cn>
2018-03-06 11:41:00 +0800

    -4

--- zufang-service/src/main/java/com/apass/zufang/service/house/ApartHouseService.java
+++ zufang-service/src/main/java/com/apass/zufang/service/house/ApartHouseService.java
@@ -5,12 +5,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.apass.gfb.framework.exception.BusinessException;
 import com.apass.gfb.framework.logstash.LOG;
-import com.apass.zufang.domain.entity.ApartHouseList;
+import com.apass.zufang.domain.dto.ApartHouseList;
 import com.apass.zufang.domain.entity.Apartment;
 import com.apass.zufang.domain.entity.HouseImg;
 import com.apass.zufang.domain.vo.HouseVo;
@@ -67,9 +68,14 @@ public class ApartHouseService {
 		LOG.info("查询公寓房源信息_获取公寓成功！");
 		for (int i = 0; i < resultApartment.size(); i++) {
 			List<HouseVo> houseListById = houseMapper.getHouseById(Arrays.asList(resultApartment.get(i).getId()));
+			if (ValidateUtils.listIsTrue(houseListById)) {
 			if (houseListById.size() > 4) {
 				PageBean<HouseVo> pageBean1 = new PageBean<HouseVo>(new Integer(pageNum)+1, 5, houseListById);
 				houseListById = pageBean1.getList();
+				}
+			for (HouseVo houseVo : houseListById) {
+				houseVo.setPictures(houseImgService.getImgList(resultApartment.get(i).getId(), (byte) 1));
+			}
 			}
 			ApartHouseList eachAPH = new ApartHouseList();
 			eachAPH.setId(resultApartment.get(i).getId());
@@ -77,16 +83,18 @@ public class ApartHouseService {
 			eachAPH.setArea(resultApartment.get(i).getArea());
 //			List<String> imgList = houseImgService.getImgList(resultApartment.get(i).getId(), (byte) 1);
 			eachAPH.setRows(houseListById);
-			eachAPH.setPictures(Arrays.asList(resultApartment.get(i).getCompanyLogo()));
 			apartHouseList.add(eachAPH);
 			}
 		}
 		return apartHouseList;
 	}
 	
-	public List<HouseVo> getHouseById(String houseId, String pageNum) {
+	public List<HouseVo> getHouseById(String houseId, String pageNum, String pageSize) {
 		List<HouseVo> apartList = houseMapper.getHouseById(Arrays.asList(houseId));
-		PageBean<HouseVo> pageBean = new PageBean<HouseVo>(new Integer(pageNum)+1, 20, apartList);
+		if (StringUtils.isBlank(pageSize)) {
+			pageSize = "20";
+		}
+		PageBean<HouseVo> pageBean = new PageBean<HouseVo>(new Integer(pageNum)+1, new Integer(pageSize), apartList);
 		apartList = pageBean.getList();
 		LOG.info("查询房源List_获取房源信息成功！");
 		for (HouseVo houseVo : apartList) {
