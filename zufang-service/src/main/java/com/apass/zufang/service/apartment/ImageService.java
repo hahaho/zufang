620df3779c40de598e2848a093f49293d229ab17
xujie <xujie@apass.cn>
2018-03-12 15:19:36 +0800

    imageuri

--- zufang-service/src/main/java/com/apass/zufang/service/apartment/ImageService.java
+++ zufang-service/src/main/java/com/apass/zufang/service/apartment/ImageService.java
@@ -1,5 +1,6 @@
 package com.apass.zufang.service.apartment;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.apass.gfb.framework.exception.BusinessException;
 /**
@@ -9,19 +10,21 @@ import com.apass.gfb.framework.exception.BusinessException;
  */
 @Service
 public class ImageService {
-    private static final String PATH = "http://zf-uat.apass.cn/static";
+
+    @Value("${zufang.image.uri}")
+    private String path;
     /**
      * @param imageUrl 加密前的图片路径
      * @return String
      */
     public String getComplateUrl(String imageUrl) throws BusinessException {
-        if (StringUtils.isBlank(PATH)) {
+        if (StringUtils.isBlank(path)) {
             throw new BusinessException("PATH为空");
         }
         if (StringUtils.isBlank(imageUrl)) {
             return null;
         } else {
-            return PATH + imageUrl;
+            return path + imageUrl;
         }
     }
 }
