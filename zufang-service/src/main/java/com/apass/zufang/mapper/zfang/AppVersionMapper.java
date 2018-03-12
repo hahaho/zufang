f913c58f7f6fba26a42f847bb20f5259aaecefce
zhanwendong <zhanwendong@apass>
2018-03-12 16:19:28 +0800

    app版本管理

--- zufang-service/src/main/java/com/apass/zufang/mapper/zfang/AppVersionMapper.java
+++ zufang-service/src/main/java/com/apass/zufang/mapper/zfang/AppVersionMapper.java
@@ -2,7 +2,7 @@ package com.apass.zufang.mapper.zfang;
 
 import com.apass.gfb.framework.mybatis.GenericMapper;
 import com.apass.gfb.framework.mybatis.page.Pagination;
-import com.apass.zufang.domain.common.AppVersionEntity;
+import com.apass.zufang.domain.entity.AppVersionEntity;
 
 public interface AppVersionMapper extends GenericMapper<AppVersionEntity, Long> {
 
