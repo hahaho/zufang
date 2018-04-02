dc829a103f98ef8eb6707830cbaf03935d13d0b3
haotian <wanghaotian@apass.cn>
2018-04-02 18:22:05 +0800

    111SQL

--- zufang-ams/src/main/java/com/apass/zufang/common/ZufangButtonJoinColler.java
+++ zufang-ams/src/main/java/com/apass/zufang/common/ZufangButtonJoinColler.java
@@ -1,9 +1,9 @@
 package com.apass.zufang.common;
-
-
+import com.apass.gfb.framework.exception.BusinessException;
 import com.apass.gfb.framework.jwt.TokenManager;
 import com.apass.gfb.framework.security.toolkit.SpringSecurityUtils;
 import com.apass.gfb.framework.utils.CommonUtils;
+import com.apass.gfb.framework.utils.GsonUtils;
 import com.apass.zufang.domain.Response;
 import com.apass.zufang.domain.constants.ConstantsUtil;
 import com.apass.zufang.domain.entity.rbac.UsersDO;
@@ -12,24 +12,27 @@ import com.apass.zufang.domain.vo.HouseVo;
 import com.apass.zufang.service.apartment.ApartmentService;
 import com.apass.zufang.service.house.HouseService;
 import com.apass.zufang.service.rbac.UsersService;
+import com.apass.zufang.utils.ValidateUtils;
 import com.apass.zufang.web.house.HouseControler;
 import com.google.common.collect.Maps;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.Map;
-
 /**
  * Created by xiaohai on 2018/3/19.
+ * 公寓使用方接口对接
+ *
  */
-@Path("")
+@Path("/api")
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class ZufangButtonJoinColler {
@@ -42,9 +45,13 @@ public class ZufangButtonJoinColler {
     private HouseService houseService;
     @Autowired
     private ApartmentService apartmentService;
-
+    /**
+     * getToken 获取token
+     * @param paramMap
+     * @return
+     */
     @POST
-    @Path("/api/token")
+    @Path("/token")
     public Response getToken(Map<String, Object> paramMap){
         String token = null;
         Map<String,Object> resultMap = Maps.newHashMap();
@@ -67,12 +74,15 @@ public class ZufangButtonJoinColler {
             LOGGER.error("获取token失败!---Exception---{}",e);
             return Response.fail(e.getMessage(),resultMap);
         }
-
         return Response.success("获取token成功",resultMap);
     }
-
+    /**
+     * createHouse 新增房源
+     * @param paramMap
+     * @return
+     */
     @POST
-    @Path("/api/house/create")
+    @Path("/house/create")
     public Response createHouse(Map<String, Object> paramMap){
         try{
             //房源信息
@@ -85,7 +95,65 @@ public class ZufangButtonJoinColler {
             LOGGER.error("添加房源信息失败!",e);
             return Response.fail("添加房源信息失败");
         }
-
         return Response.success("添加房源信息成功");
+    }
+    /**
+     * downHouse  下架
+     * @param map
+     * @return
+     */
+    @POST
+    @Path("/house/downHouse")
+    public Response downHouse(Map<String, Object> map){
+        try{
+        	LOGGER.info("downHouse house map--->{}",GsonUtils.toJson(map));
+        	String idStr = CommonUtils.getValue(map, "idStr");
+        	ValidateUtils.isNotBlank(idStr, "房屋Id字符串为空！");
+        	String strarr[] = idStr.split(",");
+        	for(String str : strarr){
+        		if(StringUtils.isNotBlank(str)){
+        			houseService.downHouse(str, SpringSecurityUtils.getCurrentUser());
+        		}
+        	}
+        	return Response.success("下架房源信息成功");
+        }catch (BusinessException e){
+        	LOGGER.error("downHouse businessException---->{}",e);
+			return Response.fail(e.getErrorDesc());
+        }catch (Exception e){
+        	LOGGER.error("downHouse Exception---->{}",e);
+            return Response.fail("下架房源信息失败!");
+        }
+    }
+    /**
+     * downHouse  上架
+     * @param map
+     * @return
+     */
+    @POST
+    @Path("/house/upHouse")
+    public Response upHouse(Map<String, Object> map){
+        try{
+        	LOGGER.info("upHouse house map--->{}",GsonUtils.toJson(map));
+        	String idStr = CommonUtils.getValue(map, "idStr");
+        	ValidateUtils.isNotBlank(idStr, "房屋Id字符串为空！");
+        	String strarr[] = idStr.split(",");
+        	StringBuffer sb = new StringBuffer();
+        	String message = null;
+        	String user = SpringSecurityUtils.getCurrentUser();
+        	for(String str : strarr){
+        		if(StringUtils.isNotBlank(str)){
+        			sb.append("房源信息：").append(str).append(",").append("上架详情：");
+        			message = houseService.upHouse(str, user);
+        			sb.append(message).append("。");
+        		}
+        	}
+        	return Response.success(sb.toString());
+        }catch (BusinessException e){
+        	LOGGER.error("upHouse businessException---->{}",e);
+			return Response.fail(e.getErrorDesc());
+        }catch (Exception e){
+        	LOGGER.error("upHouse Exception---->{}",e);
+            return Response.fail("上架房源信息失败!");
+        }
     }
 }
