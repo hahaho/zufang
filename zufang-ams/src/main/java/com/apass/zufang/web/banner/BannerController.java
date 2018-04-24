abb05ff9eb906957744a2f4c7ea8a487eec5c576
pengyingchao <pengyingchao@apass.cn>
2018-04-24 15:48:30 +0800

    banner

--- zufang-ams/src/main/java/com/apass/zufang/web/banner/BannerController.java
+++ zufang-ams/src/main/java/com/apass/zufang/web/banner/BannerController.java
@@ -17,17 +17,24 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.apass.gfb.framework.exception.BusinessException;
+import com.apass.gfb.framework.security.toolkit.SpringSecurityUtils;
+import com.apass.gfb.framework.utils.CommonUtils;
+import com.apass.gfb.framework.utils.GsonUtils;
 import com.apass.gfb.framework.utils.HttpWebUtils;
 import com.apass.gfb.framework.utils.ImageUtils;
 import com.apass.zufang.common.utils.MapEntryOrConverUtils;
 import com.apass.zufang.domain.Response;
 import com.apass.zufang.domain.dto.BannerQueryParams;
 import com.apass.zufang.domain.vo.BannerVo;
+import com.apass.zufang.service.apartment.ImageService;
 import com.apass.zufang.service.banner.BannerService;
 import com.apass.zufang.utils.ResponsePageBody;
+import com.apass.zufang.utils.ValidateUtils;
 
 @Path("/banner")
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
@@ -40,14 +47,9 @@ public class BannerController{
     private BannerService bannerService;
     
     private static final String BANNER_TYPE = "bannerType";
-    /**
-     * 图片服务器地址
-     */
-//    @Value("${nfs.rootPath}")
+    /*** 图片服务器地址*/
+    @Value("${nfs.rootPath}")
     private String rootPath;
-
-//    @Value("${nfs.banner}")
-    private String nfsBanner;
 
    
     @POST
@@ -70,6 +72,54 @@ public class BannerController{
 		}
 		
     }
+    
+    @POST
+	@Path("/addBanner")
+	public Response addBanner(Map<String, Object> paramMap){
+    	try {
+			logger.info("add/edit banner paramMap--->{}",GsonUtils.toJson(paramMap));
+			BannerVo vo = (BannerVo) MapEntryOrConverUtils.converMap(BannerVo.class, paramMap);
+			
+			ValidateUtils.isNotBlank(vo.getType(), "banner类型不能为空!");
+			ValidateUtils.isNotBlank(vo.getSort(),"banner排序不能为空!");
+			ValidateUtils.isPositiveNumber(vo.getSort(), "banner排序");
+			ValidateUtils.isNotBlank(vo.getActivityUrl(),"banner链接不能为空!");
+			ValidateUtils.isNotBlank(vo.getImgUrl(),"banner图片不能为空!");
+			
+			vo.setOperationName(SpringSecurityUtils.getCurrentUser());
+			bannerService.saveOrUpdateBanner(vo);
+			return Response.success("add banner信息成功！");
+		}catch (BusinessException e){
+			logger.error("add/edit banner businessException---->{}",e);
+			return Response.fail(e.getErrorDesc());
+		}catch (Exception e) {
+			logger.error("新增/编辑banner信息失败，错误原因", e);
+		    return Response.fail("新增/编辑banner信息失败！");
+		}
+    }
+    
+    /**
+	 * 删除房屋信息
+	 * @param paramMap
+	 * @return
+	 */
+	@POST
+	@Path("/delBanner")
+	public Response delBanner(Map<String, Object> paramMap){
+		try {
+			logger.info("del banner paramMap--->{}",GsonUtils.toJson(paramMap));
+			String id = CommonUtils.getValue(paramMap, "id");
+			ValidateUtils.isNotBlank(id, "bannerId为空！");
+			bannerService.deleteBanner(id, SpringSecurityUtils.getCurrentUser());
+			return Response.success("删除banner信息成功！");
+		}catch (BusinessException e){
+			logger.error("delete banner businessException---->{}",e);
+			return Response.fail(e.getErrorDesc());
+		}catch (Exception e) {
+			logger.error("删除banner信息失败，错误原因", e);
+		    return Response.fail("删除banner信息失败！");
+		}
+	}
 
     /**
      * 查看图片
@@ -99,150 +149,5 @@ public class BannerController{
         }
         writer.write(obj.toString());
     }
-
-    /**
-     * 添加banner信息 上传图片文件
-     * 
-     * @param pageModel
-     * @return
-     */
-//    @ResponseBody
-//    @RequestMapping(value = "/addBannerFile", method = RequestMethod.POST)
-//    @LogAnnotion(operationType = "添加banner信息", valueType = LogValueTypeEnum.VALUE_DTO)
-//    public Response addBannerInfor(AddBannerInfoEntity pageModel) {
-//        Banner entity = new Banner();
-//        try {
-//            String bannerName = pageModel.getBannerName();
-//            String bannerType = pageModel.getBannerType();
-//            String bannerOrder = pageModel.getBannerOrder();
-//            String activityUrl = pageModel.getActivityUrl();
-//            String activityName =pageModel.getActivityName();
-//            if (StringUtils.isAnyBlank(bannerType, bannerOrder) || !StringUtils.isNumeric(bannerOrder)) {
-//                throw new BusinessException("参数有误.");
-//            }
-//
-//            if (pageModel.getBannerFile().getInputStream().available() == 0 && pageModel.getBannerId() == null) {
-//                throw new BusinessException("请选择文件.");
-//            }
-//            //电商3期511 20170519 banner 添加设置商品链接
-//            if(StringUtils.isNotBlank(activityUrl)){
-//            	if("activity".equals(activityName)){
-//                  entity.setAttrVal(activityUrl);
-//            		activityUrl="ajqh://cn.apass.ajqh/web?url="+activityUrl;
-//                  entity.setAttr("activity");
-//            	}else if("goodId".equals(activityName)){
-//                  //这里由原来的goodId 改为 商品编号或skuid
-//                  entity.setAttrVal(activityUrl);
-//                  //GoodsBasicInfoEntity goodsInfo=goodsService.getByGoodsBySkuIdOrGoodsCode2(activityUrl, SourceType.WZ);
-//                  GoodsBasicInfoEntity goodsInfo=goodsService.getByGoodsBySkuIdOrGoodsCode2(activityUrl);
-//                  if(goodsInfo == null){
-//                      return Response.fail("请添加已上架的商品");
-//                  }
-//            		if(StringUtils.isNotBlank(goodsInfo.getSource()) ){
-//            			activityUrl="ajqh://cn.apass.ajqh/goods?id="+goodsInfo.getGoodId()+"&source=jd";
-//            		}else{
-//            			activityUrl="ajqh://cn.apass.ajqh/goods?id="+goodsInfo.getGoodId()+"&source=notJd";
-//            		}
-//                  entity.setAttr("goodId");
-//
-//            		
-//            	}
-//
-//                entity.setActivityUrl(activityUrl);
-//            }
-//            entity.setBannerName(bannerName);
-//            entity.setBannerType(bannerType);
-//            entity.setBannerOrder(Long.valueOf(bannerOrder));
-//
-//            //图片验证
-//            MultipartFile file = pageModel.getBannerFile();
-//            if(file.getInputStream().available() >0){
-//                String imgType = ImageTools.getImgType(file);
-//                String fileName = FILE_NAME_PREFIX + bannerOrder + "_" + System.currentTimeMillis() + "." + imgType;
-//                String fileUrl = nfsBanner + bannerType + "/" + fileName;
-//
-//                boolean checkHomePageBannerImgSize = false;
-//                if("index".equals(bannerType)){
-//                    checkHomePageBannerImgSize = ImageTools.checkHomePageBannerImgSize(file);
-//                }else{
-//                    checkHomePageBannerImgSize = ImageTools.checkHomePageBannerImgSizeForSift(file);
-//                }
-//                boolean checkImgType = ImageTools.checkImgType(file);// 尺寸
-//                int size = file.getInputStream().available();
-//
-//                if (!(checkHomePageBannerImgSize && checkImgType)) {// 750*420;.png,.jpg
-//                    file.getInputStream().close();
-//                    if("index".equals(bannerType)){
-//                        return Response.fail("文件尺寸不符,上传图片尺寸必须是宽：750px,高：300px,格式：.jpg,.png");
-//                    }else{
-//                        return Response.fail("文件尺寸不符,上传图片尺寸必须是宽：750px,高：300px,格式：.jpg,.png");
-//                    }
-//                } else if (size > 1024 * 512) {
-//                    file.getInputStream().close();
-//                    return Response.fail("文件不能大于500kb!");
-//                }
-//
-//                FileUtilsCommons.uploadFilesUtil(rootPath, fileUrl, file);
-//
-//                pageModel.setBannerFile(null);
-//                entity.setBannerImgUrl(fileUrl);
-//            }
-//
-//            Integer result = null;
-//            if(pageModel.getBannerId() == null){
-//                String respon = isAdd(bannerType, Long.valueOf(bannerOrder));
-//                if (!"ok".equals(respon)) {
-//                    return Response.fail(respon);
-//                }
-//                entity.setCreateUser(SpringSecurityUtils.getLoginUserDetails().getUsername());
-//                entity.setUpdateUser(SpringSecurityUtils.getLoginUserDetails().getUsername());
-//                result = bannerInfoService.addBannerInfor(entity);
-//                if (result == 1) {
-//                    return Response.success("上传banner成功！");
-//                } else {
-//                    return Response.fail("上传banner失败！");
-//                }
-//            }else{
-//                entity.setId(pageModel.getBannerId());
-//                entity.setUpdateUser(SpringSecurityUtils.getLoginUserDetails().getUsername());
-//                result = bannerInfoService.update(entity);
-//                if (result == 1) {
-//                    return Response.success("编辑操作成功！");
-//                } else {
-//                    return Response.fail("上传banner失败！");
-//                }
-//            }
-//
-//        } catch (BusinessException e) {
-//            LOGGER.error("上传banner失败！", e);
-//            return Response.fail(e.getErrorDesc());
-//        } catch (Exception e) {
-//            LOGGER.error("上传banner失败！", e);
-//            return Response.fail("上传banner失败！");
-//        }
-//    }
-
-    /**
-     * 判断是否可以添加图片
-     * 
-     * @param bannerType
-     * @param bannerOrder
-     * @return
-     */
-    private String isAdd(String bannerType, Long bannerOrder) {
-
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put(BANNER_TYPE, bannerType);
-//        List<Banner> banners = bannerInfoService.loadBannersList(map); // 判断是否大于等于5
-//        if (banners.size() >= 0 && banners.size() < 5) {
-//            for (Banner banner : banners) {
-//                if (bannerOrder == banner.getBannerSort()) { // 判断是否存在重复
-//                    return  "该排序值已经存在！";
-//                }
-//            }
-//        } else {
-//            return "该banner类型超过5条，请删除后再添加！";
-//        }
-        return "ok";
-    }
+    
 }
--- zufang-service/src/main/java/com/apass/zufang/service/banner/BannerService.java
+++ zufang-service/src/main/java/com/apass/zufang/service/banner/BannerService.java
@@ -1,18 +1,21 @@
 package com.apass.zufang.service.banner;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.apass.gfb.framework.exception.BusinessException;
 import com.apass.gfb.framework.utils.BaseConstants;
 import com.apass.zufang.domain.dto.BannerQueryParams;
 import com.apass.zufang.domain.entity.Banner;
 import com.apass.zufang.domain.enums.IsDeleteEnums;
 import com.apass.zufang.domain.vo.BannerVo;
-import com.apass.zufang.domain.vo.HouseBagVo;
 import com.apass.zufang.mapper.zfang.BannerMapper;
+import com.apass.zufang.service.apartment.ImageService;
 import com.apass.zufang.utils.ResponsePageBody;
 
 @Service
@@ -20,16 +23,19 @@ public class BannerService {
 
     @Autowired
     private BannerMapper bannerDao;
+    
+    @Autowired
+    private ImageService imgService;
 
-    public List<Banner> loadIndexBanners(String type) {
-        return bannerDao.loadIndexBanners(type);
-    }
-
-    /*** banner信息管理列表 */
-	public ResponsePageBody<BannerVo> getBannerListsExceptDelete(BannerQueryParams dto){
+    /*** banner信息管理列表 
+     * @throws BusinessException */
+	public ResponsePageBody<BannerVo> getBannerListsExceptDelete(BannerQueryParams dto) throws BusinessException{
 		ResponsePageBody<BannerVo> body = new ResponsePageBody<>();
 		dto.setIsDelete(IsDeleteEnums.IS_DELETE_00.getCode());
 		List<BannerVo> houseList = bannerDao.getBannerLists(dto);
+		for (BannerVo bannerVo : houseList) {
+			bannerVo.setFullImgUrl(imgService.getComplateUrl(bannerVo.getImgUrl()));
+		}
 		body.setRows(houseList);
 		body.setTotal(bannerDao.getBannerListsCount(dto));
 		body.setStatus(BaseConstants.CommonCode.SUCCESS_CODE);
@@ -57,15 +63,109 @@ public class BannerService {
 //    public Integer update(Banner entity){
 //        return bannerDao.updateByPrimaryKeySelective(entity);
 //    }
-
+	@Transactional(value="transactionManager",rollbackFor = { Exception.class,RuntimeException.class})
+	public void saveOrUpdateBanner(BannerVo banner) throws BusinessException{
+		
+		if(null == banner || StringUtils.isBlank(banner.getType())){
+			throw new BusinessException("banner信息有误!");
+		}
+		
+		/*** 1.首先根据Banner type 判断，此类型下的banner 数量是否达到5个，否则直接返回*/
+		List<Banner> banners = getBannersByType(banner.getType());
+		if(CollectionUtils.size(banners) >= 5){
+			throw new BusinessException("该banner类型下banner数量已达上限！");
+		}
+		
+		updateBannerSort(banners, banner);
+		
+		saveOrupdate(banner);//新增或者修改banner
+	}
+	
+	/**
+	 * 根据类型和排序和Id，查询出对应的banner,如果存在则说明sort有冲突，
+	 * 把之前的sort和比sort大的sort字段都加1
+	 * @param banners
+	 * @param banner
+	 */
+	@Transactional(value="transactionManager",rollbackFor = { Exception.class,RuntimeException.class})
+	public void updateBannerSort(List<Banner> banners,BannerVo banner){
+		BannerQueryParams params  = new BannerQueryParams();
+		params.setBannerType(banner.getType());
+		params.setIsDelete(IsDeleteEnums.IS_DELETE_00.getCode());
+		params.setId(banner.getId());
+		params.setSort(banner.getSort());
+		List<Banner> sameSortBanner = bannerDao.loadIndexBanners(params);
+		
+		if(CollectionUtils.isNotEmpty(sameSortBanner)){
+			for (Banner ban : banners) {
+				if(ban.getBannerSort() >= Long.parseLong(banner.getType())){
+					ban.setBannerSort(ban.getBannerSort() + 1);
+					ban.setUpdatedTime(new Date());
+					ban.setUpdateUser(banner.getOperationName());
+					bannerDao.updateByPrimaryKeySelective(ban);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * 根据type查询banner
+	 * @param type
+	 * @return
+	 */
+	public List<Banner> getBannersByType(String type){
+		BannerQueryParams params  = new BannerQueryParams();
+		params.setBannerType(type);
+		params.setIsDelete(IsDeleteEnums.IS_DELETE_00.getCode());
+		List<Banner> banners = bannerDao.loadIndexBanners(params);
+		return banners;
+	}
+	
+	/**
+	 * 新增或者修改Banner
+	 * @param banner
+	 */
+	@Transactional(value="transactionManager",rollbackFor = { Exception.class,RuntimeException.class})
+	public void saveOrupdate(BannerVo banner){
+		//新增
+		Banner record = new Banner();
+		Date date = new Date();
+		record.setActivityUrl(banner.getActivityUrl());
+		record.setBannerImgUrl(banner.getImgUrl());
+		record.setBannerType(new Byte(banner.getType()));
+		record.setBannerSort(Long.parseLong(banner.getSort()));
+		record.setUpdatedTime(date);
+		record.setUpdateUser(banner.getOperationName());
+		String bannerName = banner.getImgUrl().substring(banner.getImgUrl().lastIndexOf("/")+1);
+		record.setBannerName(bannerName);
+		
+		if(StringUtils.isBlank(banner.getId())){
+			record.setCreatedTime(date);
+			record.setCreateUser(banner.getOperationName());
+			bannerDao.insertSelective(record);
+			return;
+		}
+		record.setId(Long.parseLong(banner.getId()));
+		bannerDao.updateByPrimaryKeySelective(record);
+	}
     /**
-     * 删除
+     * 删除banner(逻辑删除)
      */
-//    @Transactional(rollbackFor = Exception.class)
-//    public Integer deleteBannerInfor(Long id) {
-//        return bannerDao.deleteByPrimaryKey(id);
-//    }
-
+	@Transactional(value="transactionManager",rollbackFor = { Exception.class,RuntimeException.class})
+	public void deleteBanner(String id,String updateUser) throws BusinessException{
+		if(StringUtils.isBlank(id)){
+			throw new BusinessException("bannerId不能为空!");
+		}
+		Banner banner = bannerDao.selectByPrimaryKey(Long.parseLong(id));
+		if(null == banner){
+			throw new BusinessException("id为{}的banner的不存在!",id);
+		}
+		banner.setUpdatedTime(new Date());
+		banner.setUpdateUser(updateUser);
+		banner.setIsDelete(IsDeleteEnums.IS_DELETE_01.getCode());
+		bannerDao.updateByPrimaryKeySelective(banner);
+		
+	}
     /**
      * 新增
      * @param entity
