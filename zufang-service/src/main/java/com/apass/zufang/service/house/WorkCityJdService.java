7f3c882bd9ebe22801c5ab961adb668357d03d97
xujie <xujie@apass.cn>
2018-04-24 15:00:37 +0800

    根据city查询省

---
+++ zufang-service/src/main/java/com/apass/zufang/service/house/WorkCityJdService.java
@@ -0,0 +1,33 @@
+package com.apass.zufang.service.house;
+
+import com.apass.gfb.framework.exception.BusinessException;
+import com.apass.zufang.domain.common.WorkCityJd;
+import com.apass.zufang.mapper.zfang.WorkCityJdMapper;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by DELL on 2018/4/24.
+ */
+@Service
+public class WorkCityJdService {
+
+    @Autowired
+    private WorkCityJdMapper workCityJdMapper;
+
+    public WorkCityJd getByCityName(String city) throws BusinessException{
+       WorkCityJd workCityJd = workCityJdMapper.selectByCityName(city);
+       if(workCityJd == null){
+           throw new BusinessException("未查询到该省份，city = " + city);
+       }
+        List<WorkCityJd> list= workCityJdMapper.selectDateByParentId(workCityJd.getParent().toString());
+        if(CollectionUtils.isEmpty(list)){
+            throw new BusinessException("未查询到该省份，city = " + city);
+        }
+        return list.get(0);
+    }
+
+}
