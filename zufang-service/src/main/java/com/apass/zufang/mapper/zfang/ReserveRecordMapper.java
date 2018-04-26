bffdb39f18fe16c84ff451a8cb999b7fcc45b8dd
haotian <wanghaotian@apass.cn>
2018-04-26 14:41:39 +0800

    预约看房记录变更表  实体类

---
+++ zufang-service/src/main/java/com/apass/zufang/mapper/zfang/ReserveRecordMapper.java
@@ -0,0 +1,10 @@
+package com.apass.zufang.mapper.zfang;
+import com.apass.gfb.framework.mybatis.GenericMapper;
+import com.apass.zufang.domain.entity.ReserveRecord;
+/**
+ * @author xiaohai
+ * @date 2018/4/26.
+ */
+public interface ReserveRecordMapper extends GenericMapper<ReserveRecord,Long> {
+
+}
