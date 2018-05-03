516bebf290ba2f3e806a0be028365f4adf76492c
haotian <wanghaotian@apass.cn>
2018-05-03 17:04:40 +0800

    zaixianyuyue

--- zufang-service/src/main/java/com/apass/zufang/service/appointment/OnlineAppointmentService.java
+++ zufang-service/src/main/java/com/apass/zufang/service/appointment/OnlineAppointmentService.java
@@ -1,5 +1,4 @@
 package com.apass.zufang.service.appointment;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import org.slf4j.Logger;
@@ -9,8 +8,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import com.apass.gfb.framework.exception.BusinessException;
 import com.apass.gfb.framework.utils.BaseConstants;
+import com.apass.gfb.framework.utils.DateFormatUtil;
 import com.apass.zufang.domain.entity.HouseShowingsEntity;
 import com.apass.zufang.domain.entity.ReserveHouse;
+import com.apass.zufang.domain.entity.ReserveRecord;
 import com.apass.zufang.domain.vo.ReservationsShowingsEntity;
 import com.apass.zufang.mapper.zfang.ReserveHouseMapper;
 import com.apass.zufang.utils.ResponsePageBody;
@@ -22,6 +23,8 @@ public class OnlineAppointmentService {
 	private static final Logger LOGGER = LoggerFactory.getLogger(OnlineAppointmentService.class);
 	@Autowired
 	private ReserveHouseMapper reserveHouseMapper;
+	@Autowired
+	private ReserveRecordService reserveRecordService;
 	@Value("${zufang.image.uri}")
 	private String imageUri;
 	/**
@@ -35,16 +38,16 @@ public class OnlineAppointmentService {
 	 * @return
 	 * @throws BusinessException
 	 */
-	public Integer insetReserveHouse(String houseId, String userId, String telphone, String name, String reservedate,String memo) throws BusinessException {
+	public Integer insetReserveHouse(String houseId, String userId, String telphone, String name, String reservedate,String memo,String user) throws BusinessException {
 		ReserveHouse setreserveHouse = new ReserveHouse();
 		try {
-			java.text.SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+			Date reserveDate = DateFormatUtil.string2date(reservedate+":00",DateFormatUtil.YYYY_MM_DD_HH_MM_SS);
 			setreserveHouse.setHouseId(Long.parseLong(houseId));// 房源主键
 			setreserveHouse.setUserId(userId);// 用户ID
 			setreserveHouse.setType(new Byte("1"));
 			setreserveHouse.setTelphone(telphone);// 联系方式
 			setreserveHouse.setName(name);// 姓名
-			setreserveHouse.setReserveDate(formatter.parse(reservedate));// 看房时间
+			setreserveHouse.setReserveDate(reserveDate);// 看房时间
 			setreserveHouse.setMemo(memo);// 留言
 			setreserveHouse.setIsDelete("00");
 			setreserveHouse.setCreatedTime(new Date());
@@ -56,6 +59,17 @@ public class OnlineAppointmentService {
 			Integer selectrepeat = reserveHouseMapper.selectrepeat(telphone);
 			if(selectByPrimaryKey < 1 && selectrepeat < 5){
 				reserveHouseMapper.insert(setreserveHouse);
+				ReserveRecord record = new ReserveRecord();
+				record.setReserveHouseId(setreserveHouse.getId());
+				record.setOperateType((byte)1);//新增的看房行程 :新增的看房记录  操作类型，1:预约看房   默认状态
+				record.setReserveDate(reserveDate);
+				record.setOperateTime(new Date());
+				record.setRemark("用户第1次预约操作成功！");
+				record.setCreatedTime(new Date());
+				record.setUpdatedTime(new Date());
+				record.setCreatedUser(user);
+				record.setUpdatedUser(user);
+				reserveRecordService.createEntity(record);
 				return 1;
 			}else{
 				return 0;
